4c000a3b719d4fce34a27a3791a03b70eab813d5
J-jjp <2053930980@qq.com>
2024-05-24 07:13:39 +0800

    排球

---
+++ Volleyball/build/CMakeFiles/Volleyball_Vision.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Volleyball_Vision.dir/Volleyball_Vision.cpp.o"
+  "CMakeFiles/Volleyball_Vision.dir/Volleyball_Vision.cpp.o.d"
+  "Volleyball_Vision"
+  "Volleyball_Vision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Volleyball_Vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Volleyball/build/CMakeFiles/Volleyball_Vision.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Volleyball_Vision.
+# This may be replaced when dependencies are built.
